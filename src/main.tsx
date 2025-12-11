9ee5ba15a08136a59f69daba7a68bec1d4816efe
HovhannesJeyranyan <jeyranyanhovhannes@gmail.com>
2025-12-11 21:35:07 +0400

    Redux Toolkit Quick Start

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import App from "./App";
 import Login from "./pages/Login/Login";
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+
 
 const router = createBrowserRouter([
   {
@@ -21,6 +24,7 @@ const router = createBrowserRouter([
 const root = document.getElementById("root")!;
 
 ReactDOM.createRoot(root).render(
-  <RouterProvider router={router} />,
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
-
